11b2900c72c6c6f5f16662f79a25d0379d78db06
Sandesh2211020 <sandesh2211020@iimscollege.edu.np>
2023-03-26 13:28:42 +0545

    Basic url shortner finished

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+const database = module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+  }
+  try {
+    mongoose.connect("mongodb+srv://Vanity:YoT5tNIo3PKGbkCY@shorturl.f7lgzws.mongodb.net/test", connectionParams)
+    console.log('Connected to database ')
+  } catch (error) {
+    console.log('Could not connect to database: ', error)
+  }
+}
+
+database();
+
+// mongoose.connect("mongodb://localhost/urlShortener", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log("Connected to MongoDB");
+// }).catch((error) => {
+//   console.log(error);
+// });
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await  ShortUrl.find();
+  res.render("index", {shortUrls : shortUrls});
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.status(404).json({ error: "Url not found" });
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on http://localhost:5000/");
+});
